8f0ceba31643307b1a65db6501434d15c0213236
navadon <navadon@M-MBA.local>
2015-05-10 18:36:36 -0500

    Update Plot 1

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
 # plot1.R
 
 data <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
-hist(as.numeric(data$Global_active_power)) 
+hist(as.numeric(data$Global_active_power), col="red") 
