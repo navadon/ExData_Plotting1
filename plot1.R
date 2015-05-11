a75f265fa06e4a2a6161073a7f964dee50af1b5a
navadon <navadon@M-MBA.local>
2015-05-11 11:31:42 -0500

    Modify Plot 1

--- plot1.R
+++ plot1.R
@@ -2,6 +2,5 @@
 
 data <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 index <- c(which(data$Date == "1/2/2007"), which(data$Date == "2/2/2007"))
-# pdata <- as.numeric(data[index, 3])
-pdata <- as.numeric(data$Global_active_power[index])
+pdata <- as.double(paste(data$Global_active_power[index]))
 hist(pdata, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
