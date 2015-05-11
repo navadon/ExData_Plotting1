ab7b91f1674a73d097f689f3e4bc25a63fff9bae
navadon <navadon@M-MBA.local>
2015-05-11 13:30:35 -0500

    Modify Plot 1
    Add dimensions

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@
 hpcdata <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 index <- c(which(hpcdata$Date == "1/2/2007"), which(hpcdata$Date == "2/2/2007"))
 pdata <- as.double(paste(hpcdata$Global_active_power[index]))
-hist(pdata, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
 
-dev.copy(png, file = "plot1.png") 
+png(filename="plot1.png", width = 480, height = 480, units = "px")
+hist(pdata, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()
