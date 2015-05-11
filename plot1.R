b04f14c4f1e0434eda0b3dfff77baeb6662ca3a7
navadon <navadon@M-MBA.local>
2015-05-11 12:36:41 -0500

    Update Plot 1 files

--- plot1.R
+++ plot1.R
@@ -1,6 +1,9 @@
 # plot1.R
 
-data <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
-index <- c(which(data$Date == "1/2/2007"), which(data$Date == "2/2/2007"))
-pdata <- as.double(paste(data$Global_active_power[index]))
+hpcdata <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+index <- c(which(hpcdata$Date == "1/2/2007"), which(hpcdata$Date == "2/2/2007"))
+pdata <- as.double(paste(hpcdata$Global_active_power[index]))
 hist(pdata, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
+
+dev.copy(png, file = "plot1.png") 
+dev.off()
