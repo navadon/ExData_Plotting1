28c67672b5822722cbb2e246f1fcddb54107c570
navadon <navadon@M-MBA.local>
2015-05-11 21:04:48 -0500

    Update Plot 2
    More flexible

--- plot2.R
+++ plot2.R
@@ -1,12 +1,31 @@
 # plot2.R
 
+# read data
 hpcdata <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
-index <- c(which(hpcdata$Date == "1/2/2007"), which(hpcdata$Date == "2/2/2007"))
-pdata <- as.numeric(paste(hpcdata$Global_active_power[index]))
-# ddata <- paste(as.Date(hpcdata$Date[index],"%d/%m/%Y"))
 
+# subset data
+date1 <- "1/2/2007"
+date2 <- "2/2/2007"
+idx1 <- which(hpcdata$Date == date1)
+idx2 <- which(hpcdata$Date == date2)
+index <- c(idx1, idx2)
+pdata <- as.numeric(paste(hpcdata$Global_active_power[index]))
+
+# Graphics device for PNG
 png(filename="plot2.png", width = 480, height = 480, units = "px")
+
+# plot data
 plot(pdata,type="l",xaxt = "n",xlab="",ylab="Global Active Power (kilowatts)")
-axis(1, at=c(0,1440,2880), labels=c("Thu", "Fri", "Sat"))
+
+# set x-axis tickmark locations
+xaxisat <- c(0,length(idx1),(length(idx1)+length(idx2)))
+
+# set x-axis labels
+d1 <- as.Date(date1,"%d/%m/%Y")
+xaxislabels <- weekdays(c(d1,d1+1,d1+2),abbreviate=TRUE)
+
+# apply x-axis settings
+axis(1, at=xaxisat, labels=xaxislabels)
+
 dev.off()
 
